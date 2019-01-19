192cbf7808b371eeb5607b897b5db1c09cfdc81e
sidhant007 <sidhbansal@gmail.com>
2019-01-20 03:13:02 +0800

    integrated

--- chromeEyeDropper-master/edropper2.js
+++ chromeEyeDropper-master/edropper2.js
@@ -2,6 +2,8 @@ var EDROPPER_VERSION = 11;
 const CANVAS_MAX_SIZE = 32767 - 20
 const DEBUG = false;
 
+var colorArray = [], myWidth, myHeight;
+
 //console.log($(document).width(), " : ", $(document).height() )
 var page = {
     width: 818,
@@ -30,7 +32,7 @@ var page = {
     // ---------------------------------
     messageListener: function() {
         // Listen for pickup activate
-        console.log('dropper: page activated');
+        //console.log('dropper: page activated');
         chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
             switch (req.type) {
                 case 'edropper-version':
@@ -47,7 +49,7 @@ var page = {
                     page.dropperDeactivate();
                     break;
                 case 'update-image':
-                    console.log('dropper: background send me updated screenshot');
+                //console.log('dropper: background send me updated screenshot');
                     page.imageData = req.data;
                     page.capture();
                     break;
@@ -98,7 +100,7 @@ var page = {
             page.elColorToolboxText = $('#color-toolbox-text');
         }
 
-        console.log('dropper: activating page dropper');
+      //console.log('dropper: activating page dropper');
         page.defaults();
 
         page.dropperActivated = true;
@@ -153,7 +155,7 @@ var page = {
     onMouseMove: function(e) {
         if (!page.dropperActivated)
             return;
-        console.log("printing here:", e)
+      //console.log("printing here:", e)
         page.tooltip(e);
     },
 
@@ -174,7 +176,7 @@ var page = {
         if (!page.dropperActivated)
             return;
 
-        console.log("dropper: Scroll stop");
+      //console.log("dropper: Scroll stop");
         page.screenChanged();
     },
 
@@ -219,7 +221,7 @@ var page = {
         if (!page.dropperActivated)
             return;
 
-        console.log('dropper: window resized');
+      //console.log('dropper: window resized');
 
         // set defaults
         page.defaults();
@@ -257,12 +259,14 @@ var page = {
 
         // set tooltip
         if (page.options.enableColorTooltip === true) {
+          /*
             page.elColorTooltip.css({
                 'background-color': '#' + color.rgbhex,
                 'top': e.pageY + fromTop,
                 'left': e.pageX + fromLeft,
                 'border-color': '#' + color.opposite
             }).show();
+            */
         }
 
         // set toolbox
@@ -271,7 +275,7 @@ var page = {
                 'background-color': '#' + color.rgbhex
             });
             page.elColorToolboxText.html('#' + color.rgbhex + '<br />rgb(' + color.r + ',' + color.g + ',' + color.b + ')');
-            page.elColorToolbox.show();
+          //page.elColorToolbox.show();
         }
     },
 
@@ -343,8 +347,8 @@ var page = {
             return;
 
         var canvasIndex = (e.pageX + e.pageY * page.canvas.width) * 4;
-        ////console.log(e.pageX + ' ' + e.pageY + ' ' + page.canvas.width);
-        console.log("printing event e: ", e.offsetX, e.offsetY)
+        //console.log(e.pageX + ' ' + e.pageY + ' ' + page.canvas.width);
+      //console.log("printing event e: ", e.offsetX, e.offsetY)
 
         var color = {
             r: page.canvasData[canvasIndex],
@@ -354,7 +358,7 @@ var page = {
         };
 
         color.rgbhex = page.rgbToHex(color.r, color.g, color.b);
-        ////console.log(color.rgbhex);
+        //console.log(color.rgbhex);
         color.opposite = page.rgbToHex(255 - color.r, 255 - color.g, 255 - color.b);
         return color;
     },
@@ -368,7 +372,7 @@ var page = {
             return;
 
         var canvasIndex = (myPageX + myPageY * page.canvas.width) * 4;
-        ////console.log(e.pageX + ' ' + e.pageY + ' ' + page.canvas.width);
+        //console.log(e.pageX + ' ' + e.pageY + ' ' + page.canvas.width);
         //console.log("printing event e: ", e.offsetX, e.offsetY)
 
         var color = {
@@ -379,7 +383,7 @@ var page = {
         };
 
         color.rgbhex = page.rgbToHex(color.r, color.g, color.b);
-        ////console.log(color.rgbhex);
+        //console.log(color.rgbhex);
         color.opposite = page.rgbToHex(255 - color.r, 255 - color.g, 255 - color.b);
         return color;
     },
@@ -408,7 +412,7 @@ var page = {
     checkCanvas: function() {
         // we have to create new canvas element
         if (page.canvas.width != (page.width + page.canvasBorders) || page.canvas.height != (page.height + page.canvasBorders)) {
-            console.log('dropper: creating new canvas');
+          //console.log('dropper: creating new canvas');
             page.canvas = document.createElement('canvas');
             page.canvas.width = page.width + page.canvasBorders;
             page.canvas.height = page.height + page.canvasBorders;
@@ -422,7 +426,7 @@ var page = {
         if (!page.dropperActivated)
             return;
 
-        console.log("dropper: screenChanged");
+      //console.log("dropper: screenChanged");
         page.YOffset = $(document).scrollTop();
         page.XOffset = $(document).scrollLeft();
 
@@ -437,7 +441,7 @@ var page = {
         if (!force && page.rects.length > 0) {
             for (index in page.rects) {
                 if (page.rectInRect(rect, page.rects[index])) {
-                    console.log('dropper: already shoted, skipping');
+                  //console.log('dropper: already shoted, skipping');
                     return;
                 }
             }
@@ -447,7 +451,7 @@ var page = {
 
         $("#eye-dropper-overlay").css('cursor', 'progress')
 
-        console.log('dropper: screenshoting');
+      //console.log('dropper: screenshoting');
         // TODO: this is terrible. It have to be done better way
         if (page.options.enableColorTooltip === true && page.options.enableColorToolbox === true) {
             page.elColorTooltip.hide(1, function() {
@@ -480,7 +484,7 @@ var page = {
     // capture actual Screenshot
     capture: function() {
         page.checkCanvas();
-        ////console.log(page.rects);
+        //console.log(page.rects);
 
         //    var image = new Image();
         var image = document.createElement('img');
@@ -488,7 +492,7 @@ var page = {
         image.onload = function() {
             var w= page.screenWidth = image.width;
             var h= page.screenHeight = image.height;
-            
+
 
             var rect = {
                 x: page.XOffset,
@@ -498,14 +502,14 @@ var page = {
             };
             var merged = false;
 
-            
+
 
             var myOffsetX = rect.x;
             var myOffsetY = rect.y;
-            var myWidth =rect.width;
-            var myHeight = rect.height;
-			var colorArray = new Array(myWidth);
-			var heightArray = new Array(myHeight);
+            myWidth =rect.width;
+            myHeight = rect.height;
+			      colorArray = new Array(myWidth);
+			      var heightArray = new Array(myHeight);
             //image width and height are fixed
             //xoffset is always 0
             //yoffset is scroll dependent
@@ -515,9 +519,9 @@ var page = {
                 // try to merge shot with others
                 for (index in page.rects) {
                     var t = page.rectMerge(rect, page.rects[index]);
-                    console.log("Checking index value: ", index)
+                  //console.log("Checking index value: ", index)
                     if (t != false) {
-                        console.log('dropper: merging');
+                      //console.log('dropper: merging');
                         merged = true;
                         page.rects[index] = t;
                     }
@@ -553,15 +557,17 @@ var page = {
                 var2++;
                 var1=0;
             }
-            console.log("Trying colour array: ", colorArray);
+            console.log("Printing colour array: ", colorArray);
             console.log("Percentage white: ", cnt / (myWidth * myHeight));
+
+            start_game();
 
             // re-enable tooltip and toolbox
             if (page.options.enableColorTooltip === true) {
-                page.elColorTooltip.show(1);
+              //page.elColorTooltip.show(1);
             }
             if (page.options.enableColorToolbox === true) {
-                page.elColorToolbox.show(1);
+              // page.elColorToolbox.show(1);
             }
 
             if ( DEBUG ) {
@@ -594,4 +600,356 @@ page.init();
 
 window.onresize = function() {
     page.onWindowResize();
+}
+
+/*
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+}
+*/
+/*
+var image = document.createElement("img");
+image.setAttribute("style", "margin-left:200px;");
+image.setAttribute("style", "margin-top:200px;");
+document.getElementsByTagName("body")[0].appendChild(image);
+*/
+document.documentElement.style.height = '100%';
+document.body.style.height = '100%';
+document.documentElement.style.width = '100%';
+document.body.style.width = '100%';
+
+var pacman, score_card, p_text, score_text;
+var dots = [], walls = [];
+var powers = [];
+var dot_sz = 0, wall_sz = 0, power_sz = 0;
+var score = 0;
+
+function add_score_card() {
+  score_card = document.createElement('div');
+  score_card.id = 'score_card';
+  score_card.style.position = 'fixed';
+  score_card.style.top = '100px';
+  score_card.style.left = '1250px';
+  score_card.style.width = '180px';
+  score_card.style.height = '70px';
+  document.body.appendChild(score_card);
+  score_card.style.backgroundColor = 'lime';
+  p_text = document.createElement('p');
+  p_text.style.fontSize = "30px";
+  p_text.appendChild(document.createTextNode("Score: " + score));
+  score_card.appendChild(p_text);
+  /*
+  progress_bar = document.createElement('span');
+  progress_bar.id = 'progress_bar';
+  document.body.appendChild(progress_bar);
+  */
+}
+
+function add_pacman(x, y, sz) {
+  pacman = document.createElement('img');
+  document.body.appendChild(pacman);
+  pacman.src = "https://imgur.com/ybQwK4F.gif";
+  pacman.id = 'packyboi';
+  pacman.style.position = 'fixed';
+  pacman.style.top = y + 'px'
+  pacman.style.left = x + 'px';
+  pacman.style.width = sz + 'px';
+  pacman.style.height = sz + 'px';
+}
+
+function add_dots(x, y, sz) {
+  dots[dot_sz] = document.createElement('img');
+  document.body.appendChild(dots[dot_sz]);
+  dots[dot_sz].src = "https://imgur.com/nzZOdmI.gif";
+  dots[dot_sz].id = dot_sz + '_dot';
+  dots[dot_sz].style.position = 'fixed';
+  dots[dot_sz].style.top = y + 'px';
+  dots[dot_sz].style.left = x + 'px';
+  dots[dot_sz].style.width = sz + 'px';
+  dots[dot_sz].style.height = sz + 'px';
+  dot_sz++;
+}
+
+function add_power(x, y, sz) {
+  powers[power_sz] = document.createElement('img');
+  document.body.appendChild(powers[power_sz]);
+  powers[power_sz].src = "https://imgur.com/GG9OdaD.gif"
+  powers[power_sz].id = power_sz + '_power';
+  powers[power_sz].style.position = 'fixed';
+  powers[power_sz].style.top = y + 'px';
+  powers[power_sz].style.left = x + 'px';
+  powers[power_sz].style.width = sz + 'px';
+  powers[power_sz].style.height = sz + 'px';
+  power_sz++;
+}
+
+function add_walls(x1, y1, x2, y2) {
+  walls[wall_sz] = {x1: x1, y1: y1, x2: x2, y2: y2};
+  wall_sz++;
+}
+
+//set attributes for btnForm
+//btnForm.action = '';
+
+//set attributes for btn
+//"btn.removeAttribute( 'style' );
+/*
+btn.type = 'button';
+btn.value = 'hello';
+btn.style.position = 'absolute';
+tn.style.top = '50%';
+btn.style.left = '50%';
+*/
+var speed = 8, max_speed = 20, acc = 0, prev_dir = -1, delta = 0;
+
+function to_int(x) {
+  return parseFloat(x.split('p')[0]);
+}
+
+function intersect(x1, y1, x2, y2, x3, y3, x4, y4) {
+  return !(x3 > x2 || x4 < x1 || y3 > y2 || y4 < y1);
+}
+
+function intersect_dom(a, b) {
+  var x1 = to_int(a.style.left), y1 = to_int(a.style.top);
+  var x2 = x1 + to_int(a.style.width), y2 = y1 + to_int(a.style.height);
+
+  var x3 = to_int(b.style.left), y3 = to_int(b.style.top);
+  var x4 = x3 + to_int(b.style.width), y4 = y3 + to_int(b.style.height);
+
+  return intersect(x1, y1, x2, y2, x3, y3, x4, y4);
+}
+
+function update_score(score_add) {
+  score += score_add
+  p_text.removeChild(p_text.childNodes[0]);
+  p_text.appendChild(document.createTextNode("Score: " + score));
+}
+
+function isEat(element) {
+  for(var i = 0; i < dot_sz; i++) {
+    if(intersect_dom(element, dots[i])) {
+      update_score(50);
+      dots[i].style.left = '0px';
+      dots[i].style.top = '0px';
+      dots[i].style.width = '0px';
+      dots[i].style.height = '0px';
+    }
+  }
+  for(var i = 0; i < power_sz; i++) {
+    if(intersect_dom(element, powers[i])) {
+      // time_start
+      pacman.style.width = to_int(pacman.style.width)/2 + 'px';
+      pacman.style.height = to_int(pacman.style.height)/2 + 'px';
+      setTimeout(function(){
+        pacman.style.width = to_int(pacman.style.width)*2 + 'px';
+        pacman.style.height = to_int(pacman.style.height)*2 + 'px';
+      }, 10000);
+      /*
+      var timeleft = 10;
+      var download_timer = setInterval(function() {
+        timeleft--;
+        document.getElementById("progress_bar").textContent = timeleft;
+        if(timeleft <= 0)   clearInterval(download_timer);
+      });
+      */
+      powers[i].style.left = '0px';
+      powers[i].style.top = '0px';
+      powers[i].style.width = '0px';
+      powers[i].style.height = '0px';
+    }
+  }
+}
+function isHit2(x1, y1, x2, y2) {
+  var c_width = myWidth / MAX_X, c_height = myHeight / MAX_Y;
+  var cntr = 0;
+  for(var tmp_x = x1; tmp_x <= x2; tmp_x++) {
+    for(var tmp_y = y1; tmp_y <= y2; tmp_y++) {
+      var tmp2_x = Math.round(tmp_x * c_width);
+      var tmp2_y = Math.round(tmp_y * c_height);
+      //console.log("C-x --> ", tmp_x, tmp2_x, tmp_y, tmp2_y, colorArray[tmp2_x][tmp2_y]);
+      if(colorArray[tmp_x][tmp_y] != "ffffff")  cntr++;
+    }
+  }
+  var tolerance = cntr / ((x2 - x1) * (y2 - y1));
+  if(tolerance >= 0.2)  return true;
+  return false;
+  /*
+  /*
+  for(var x_offset = -50; x_offset <= 50; x_offset++) {
+    for(var y_offset = -50; y_offset <= 50; y_offset++) {
+      for(var i = 0; i < wall_sz; i++) {
+        if(intersect(x1 + x_offset, y1 + y_offset, x2 + x_offset, y2 + y_offset, walls[i].x1, walls[i].y1, walls[i].x2, walls[i].y2)) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+  */
+}
+
+function isHit(x1, y1) {
+  x2 = x1 + to_int(pacman.style.width), y2 = y1 + to_int(pacman.style.height);
+  return isHit2(x1, y1, x2, y2);
+  /*
+  for(var i = 0; i < wall_sz; i++) {
+    if(intersect(x1, y1, x2, y2, walls[i].x1, walls[i].y1, walls[i].x2, walls[i].y2)) {
+      return true;
+    }
+  }
+  return false;
+  */
+}
+
+function leftArrowPressed() {
+  var element = document.getElementById("packyboi");
+  element.style.transform = "rotate(180deg)"
+  if(prev_dir != 0) acc = 0;acc
+  var new_left = parseFloat(element.style.left) - Math.min(max_speed, speed + acc) + 'px';
+  if(isHit(to_int(new_left), to_int(element.style.top)))  return ;
+  element.style.left = new_left;
+  acc += delta;
+  prev_dir = 0;
+  isEat(element);
+}
+
+function rightArrowPressed() {
+  var element = document.getElementById("packyboi");
+  element.style.transform = "rotate(0deg)"
+  if(prev_dir != 1) acc = 0;
+  var new_left = parseFloat(element.style.left) + Math.min(max_speed, speed + acc) + 'px';
+  if(isHit(to_int(new_left), to_int(element.style.top)))  return ;
+  element.style.left = new_left;
+  acc += delta;
+  prev_dir = 1;
+  isEat(element)
+}
+
+function upArrowPressed() {
+  var element = document.getElementById("packyboi");
+  element.style.transform = "rotate(270deg)"
+  if(prev_dir != 2) acc = 0;
+  var new_top = parseFloat(element.style.top) - Math.min(max_speed, speed + acc) + 'px';
+  if(isHit(to_int(element.style.left), to_int(new_top)))  return ;
+  element.style.top = new_top;
+  acc += delta;
+  prev_dir = 2;
+  isEat(element)
+}
+
+function downArrowPressed() {
+  var element = document.getElementById("packyboi");
+  element.style.transform = "rotate(90deg)"
+  if(prev_dir != 3) acc = 0;
+  var new_top = parseFloat(element.style.top) + Math.min(max_speed, speed + acc) + 'px';
+  if(isHit(to_int(element.style.left), to_int(new_top)))  return ;
+  element.style.top = new_top;
+  acc += delta;
+  prev_dir = 3;
+  isEat(element)
+}
+
+var game_started = true;
+
+function moveSelection(evt) {
+  if(evt.keyCode == 80 && game_started == false) { //press P to start
+    start_game();
+    game_started = true;
+  }
+  if(!game_started) return ;
+  switch (evt.keyCode) {
+    case 65:
+      leftArrowPressed();
+      break;
+    case 68:
+      rightArrowPressed();
+      break;
+    case 87:
+      upArrowPressed();
+      break;
+    case 83:
+      downArrowPressed();
+      break;
+    case 82: // R key for Respawn
+      respawn();
+      break;
+  }
+};
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var MAX_X;
+var MAX_Y;// = document.documentElement.clientHeight;
+var PACMAN_SZ = 16, DOT_SZ = 16, DOTS_CNT = 20, POWER_CNT = 5;
+
+function respawn() {
+  console.log("BC --> ", MAX_X, MAX_Y);
+  var rnd_x = getRandomInt(0, MAX_X), rnd_y = getRandomInt(0, MAX_Y);
+  while(isHit2(rnd_x, rnd_y, rnd_x + PACMAN_SZ, rnd_y + PACMAN_SZ)) {
+    rnd_x = getRandomInt(0, MAX_X), rnd_y = getRandomInt(0, MAX_Y);
+  }
+  pacman.style.top = rnd_y + 'px'
+  pacman.style.left = rnd_x + 'px';
+  update_score(-10);
+}
+
+window.addEventListener('keydown', moveSelection);
+function start_game() {
+
+  MAX_X = document.documentElement.clientWidth;
+  MAX_Y = document.documentElement.clientHeight;
+  // Adding score_card + PACMAN
+  add_score_card();
+  var rnd_x = getRandomInt(0, MAX_X), rnd_y = getRandomInt(0, MAX_Y);
+  while(isHit2(rnd_x, rnd_y, rnd_x + PACMAN_SZ, rnd_y + PACMAN_SZ)) {
+    rnd_x = getRandomInt(0, MAX_X), rnd_y = getRandomInt(0, MAX_Y);
+  }
+  add_pacman(rnd_x, rnd_y, PACMAN_SZ);
+
+  // Adding normal dots
+  for(var i = 0; i < DOTS_CNT; i++) {
+    rnd_x = getRandomInt(0, MAX_X), rnd_y = getRandomInt(0, MAX_Y);
+    while(isHit2(rnd_x, rnd_y, rnd_x + DOT_SZ, rnd_y + DOT_SZ)) {
+      rnd_x = getRandomInt(0, MAX_X), rnd_y = getRandomInt(0, MAX_Y);
+    }
+    add_dots(rnd_x, rnd_y, DOT_SZ);
+  }
+
+  // Adding power up points
+  for(var i = 0; i < POWER_CNT; i++) {
+    rnd_x = getRandomInt(0, MAX_X), rnd_y = getRandomInt(0, MAX_Y);
+    while(isHit2(rnd_x, rnd_y, rnd_x + DOT_SZ, rnd_y + DOT_SZ)) {
+      rnd_x = getRandomInt(0, MAX_X), rnd_y = getRandomInt(0, MAX_Y);
+    }
+    add_power(rnd_x, rnd_y, DOT_SZ);
+  }
+
+  function area_of(rect) {
+    return (rect.right - rect.left) * (rect.bottom - rect.top);
+  }
+  /*
+  var taglist = ["P", "A", "BUTTON", "SUMMARY", "SVG", "DETAILS"];
+  var all = document.getElementsByTagName("*");
+  for (var i = 0; i < all.length; i++) {
+    var rect = all[i].getBoundingClientRect();
+    var valid = false;
+    //if(all[i].name == "score_card") continue;
+    for(var j = 0; j < taglist.length; j++) {
+      if(all[i].tagName == taglist[j])  valid = true;
+    }
+    if(valid == false)  continue;
+    if(Math.round(area_of(rect)) > 30000) continue;
+    add_walls(Math.round(rect.left), Math.round(rect.top), Math.round(rect.right), Math.round(rect.bottom));
+  }
+  */
+  //pacman.style.backgroundColor = 'red';
 }
